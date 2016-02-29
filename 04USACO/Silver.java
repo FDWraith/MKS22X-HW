9c6ed3491b11ad088d7cf7afd03853175c130a47
Kevin Zhang <kevinzhang3702@gmail.com>
2016-02-29 18:14:01 -0500

    Tested Silver and it works on all the given test cases

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -5,47 +5,56 @@ public class Silver {
     private int[][]board2;
     private int len;
     private int startX,startY,endX,endY;
-
+    private static final boolean DEBUG = true;
+    
     public Silver(String txt){
 	try{
 	    File f = new File(txt);
 	    Scanner in = new Scanner(f);
-	    int r = in.nextInt();
-	    int c = in.nextInt();
-	    len = in.nextInt();
+	    Scanner first = new Scanner(in.nextLine());
+	    int r = first.nextInt();
+	    int c = first.nextInt();
+	    len = first.nextInt();
 	    board1 = new int[r][c];
 	    board2= new int[r][c];
 	    for(int i =0;i<board1.length;i++){
-		Scanner n = new Scanner(in.nextLine());
-		for(int j=0;j<board1.length;j++){
-		    if(n.next().equals(".")){
+		String n = in.nextLine();
+		for(int j=0;j<board1[i].length;j++){
+		    if(n.charAt(j) == '.'){
 			board1[i][j] = 0;
-		    }else if(n.next().equals("*")){
+		    }else if(n.charAt(j) == '*'){
 			board1[i][j] = -1;
 		    }
 		}
 	    } 
-	    startX = in.nextInt();
-	    startY = in.nextInt();
-	    endX = in.nextInt();
-	    endY = in.nextInt();
+	    startX = in.nextInt()-1;
+	    startY = in.nextInt()-1;
+	    endX = in.nextInt()-1;
+	    endY = in.nextInt()-1;
 	}catch(FileNotFoundException e){
 	    System.out.println("File not found");
 	}
     }
 
     public String toString(){
-	return calcAnswer(len)+"6,Zhang,Kevin";
+	String end = "";
+	for(int i=0;i<board1.length;i++){
+	    for(int j=0;j<board1[i].length;j++){
+		end += board1[i][j]+" ";
+	    }
+	    end += "\n";
+	}
+	return end;
     }
 
     public void update(){
 	for(int i=0;i<board1.length;i++){
 	    for(int j=0;j<board1[i].length;j++){
 		int sum = 0;
-		if(i-1 > 0 && board1[i-1][j]!=-1){
+		if(i-1 >= 0 && board1[i-1][j]!=-1){
 		    sum += board1[i-1][j];
 		}
-		if(j-1 > 0 && board1[i][j-1]!=-1){
+		if(j-1 >= 0 && board1[i][j-1]!=-1){
 		    sum += board1[i][j-1];
 		}
 		if(i+1 < board1.length && board1[i+1][j]!=-1){
@@ -54,24 +63,32 @@ public class Silver {
 		if(j+1 < board1[i].length && board1[i][j+1]!=-1){
 		    sum += board1[i][j+1];
 		}
-		board2[i][j] = sum;
+		
+		if(board1[i][j]!=-1){
+		    board2[i][j] = sum;
+		}else{
+		    board2[i][j] = -1;
+		}
 	    }
 	}//First for loop to calculate values
 	for(int i =0;i<board1.length;i++){
 	    for(int j=0;j<board1[i].length;j++){
 		board1[i][j] = board2[i][j];
 	    }
-	}//Second for loop is to copy values over	
+	}//Second for loop is to copy values over
+	if(DEBUG){
+	    System.out.println(this);
+	}
     }
 
-    public int calcAnswer(int k){
+    public int calcAnswer(){
 	//Initialize the first move(when k =0)
 
 	board1[startX][startY]=1;
 
 	//Loop for k-iterations, updating each time
 
-	for(int i=0;i<k;i++){
+	for(int i=0;i<len;i++){
 	    update();
 	}
 
@@ -80,8 +97,13 @@ public class Silver {
 	return board1[endX][endY];
     }
 
+    public void printSolution(){
+	System.out.println(calcAnswer()+",6,Zhang,Kevin");
+    }
+
     public static void main(String[]args){
 	Silver test = new Silver("ctravel.in");
 	System.out.println(test);
+	test.printSolution();
     }
 }
