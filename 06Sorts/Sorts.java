d5f91e6ec981f868beac95786c7e71c5e775ef85
Kevin Zhang <kevinzhang3702@gmail.com>
2016-03-06 18:25:18 -0500

    Finished MergeSort. The algo took me way too long

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -35,7 +35,7 @@ public class Sorts{
     
     public static void mergeSort(int[]arry){
 	int[]v1 = new int[arry.length];
-	mergeSort(arry,0,arry.length/2+1,arry.length/2+1,arry.length,v1);
+	mergeSort(arry,0,arry.length/2+arry.length%2,arry.length/2+arry.length%2,arry.length,v1);
 	for(int i=0;i<v1.length;i++){
 	    arry[i] = v1[i];
 	}//copy values in v1 to arry
@@ -46,8 +46,11 @@ public class Sorts{
     public static void merge(int[]old,int start1, int end1, int start2, int end2, int[]nw){
 	int first = start1;
 	int second = start2;
-        for(int i=0;i<old.length;i++){
-	    if(first != end1 && second != end2){
+	for(int i=0;i<start1;i++){
+	    nw[i]=old[i];
+	}
+	for(int i=start1;i<end2;i++){
+	    if(first < end1 && second < end2){
 		if(old[first]<old[second]){
 		    nw[i]=old[first];
 		    first++;
@@ -56,40 +59,70 @@ public class Sorts{
 		    second++;
 		}
 	    }else{
-		if(first != end1){
+		if(first < end1){
 		    nw[i]=old[first];
 		    first++;
-		}else if(second != end2){
+		}else if(second < end2){
 		    nw[i]=old[second];
 		    second++;
 		}else{
-		    return;
+		    //nw[i]=old[i];
 		}
 	    }
+	}
+        for(int i=end2;i<old.length;i++){
+	    nw[i]=old[i];
 	}
     }
 
     public static void mergeSort(int[]old,int start1, int end1, int start2, int end2, int[]nw){
-	if(start1 +1 == end1 || start2 +1 == end2){
+	/*if(start1 + 1 == end1 && start2 + 1 == end2){
 	    merge(old,start1,end1,start2,end2,nw);
 	}else{
-	    
-	    mergeSort(old,start1,end1/2+1,end1/2+1,end1,nw);
-	    mergeSort(nw,start2,end2/2+1,end2/2+1,end2,old);
+	    if(start1 + 1 == end1){
+		mergeSort(old,start2,end2/2,end2/2,end2,nw);
+		merge(nw,start1,end1,start2,end2,old);
+	    }else if(start2 + 1 == end2){
+		mergeSort(old,start1,end1/2,end1/2,end1,nw);
+		merge(nw,start1,end1,start2,end2,old);
+	    }else{
+		mergeSort(old,start1,end1/2,end1/2,end1,nw);
+		mergeSort(nw,start2,end2/2,end2/2,end2,old);
+		merge(old,start1,end1,start2,end2,nw);
+	    }
+	    }*/
+	if(start1+1==end2){//when it is a single index
+	    merge(old,start1,end1,start2,end2,nw);//copy it in.
+	}else{
+	    mergeSort(old,start1,(start1+end1)/2,(start1+end1)/2,end1,nw);
+	    mergeSort(nw,start2,(start2+end2)/2,(start2+end2)/2,end2,old);
+	    merge(old,start1,end1,start2,end2,nw);
 	}
-	merge(old,start1,end1,start2,end2,nw);
     }
 
 
     
     public static void main(String[]args){
-	int[]hello = {2,10,3,4};
+	int[]test1 = {2,1,3};
+	mergeSort(test1);
+	System.out.println(printArray(test1));
+	System.out.println();
+	
+	int[]hello = {2,-10,4,-3};
+	int[]copy = {0,0,0,0};
 	System.out.println(printArray(hello));
-	mergeSort(hello);
-	System.out.println(printArray(hello));
-	//int[]demon = {10,23,20,1,24,2,3,4,3};
-	//System.out.println(printArray(demon));
-	//mergeSort(demon);
-	//System.out.println(printArray(demon));
+	mergeSort(hello,0,hello.length/2,hello.length/2,hello.length,copy);
+	//mergeSort(hello,0,1,1,2,copy);
+	//System.out.println(printArray(copy));
+	//mergeSort(copy,2,3,3,4,hello);
+	System.out.println(printArray(copy));
+	//merge(hello,0,2,2,4,copy);
+	//System.out.println(printArray(copy));
+	System.out.println();
+	
+	int[]demon = {10,23,20,1,24,2,3,4,3};
+	System.out.println(printArray(demon));
+	mergeSort(demon);
+	System.out.println(printArray(demon));
     }
 }
