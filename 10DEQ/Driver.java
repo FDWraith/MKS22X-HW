95df22a3cd313e0955a11397c89fea514601ed4c
Kevin Zhang <kevinzhang3702@gmail.com>
2016-04-07 00:25:48 -0400

    managed to get some of DEQ working, but still some bugs to work out

--- 10DEQ/Driver.java
+++ 10DEQ/Driver.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Driver{
     public static void main(String[]args){
 	MyDeque<String> one = new MyDeque<String>();
@@ -7,6 +8,7 @@ public class Driver{
 	    one.getFirst();
 	    System.out.println("You done goofed");
 	}catch(Exception e){
+	    System.out.println("Well Done!");
 	    //e.printStackTrace();
 	}
 	try{
@@ -27,10 +29,51 @@ public class Driver{
 	}catch(Exception e){
 	    //e.printStackTrace();
 	}
+	/*
+	System.out.println(one);
+	one.addFirst("Start");
+	System.out.println(one);
+	one.removeLast();
+	System.out.println(one);
+	*/
+
+	
+	Random r = new Random();
+        for(int i = 0;i<20;i++){
+	    int op = r.nextInt(2);
+	    if(op == 0){
+		one.addFirst(""+r.nextInt(400));
+	    }else{
+		one.addLast(""+r.nextInt(400));
+	    }
+	    //
+	}
+
+	//System.out.println(one);
 	
 	
-
-
+	int counter = 0;
+ 	while(one.getSize()!= 0 && counter < 1000){
+	    int op = r.nextInt(6);
+	    if(op == 0){
+		one.addFirst(""+r.nextInt(400));
+	    }else if(op == 1){
+		one.addLast(""+r.nextInt(400));
+	    }else if(op == 2){
+		one.removeFirst();
+	    }else if(op == 3){
+		one.removeLast();
+	    }else if(op == 4){
+		one.getFirst();
+	    }else if(op == 5){
+		one.getLast();
+	    }
+	    counter++;
+	}
+	System.out.println(one);
+	System.out.println("Finished~!");
+	
     }
+    
 
 }
--- 10DEQ/MyDeque.java
+++ 10DEQ/MyDeque.java
@@ -2,25 +2,29 @@ import java.util.*;
 
 public class MyDeque<T> {
     private int size,start,end;
-    private Object[]ary;
+    private T[]ary;
 
     public MyDeque(){
-	ary = new Object[10];
+	this(10);
     }
 
     public MyDeque(int s){
-	ary = new Object[s];
+	ary = (T[])( new Object[s]);
     }
 
     public String toString(){
 	return Arrays.toString(ary) + "Start: "+start+ " End: "+end+ " Size: "+size;
     }
 
+    public int getSize(){
+	return size;
+    }
+
     private void grow(){
-        Object[]nw = new Object[ary.length*2];
+        T[]nw = (T[])(new Object[ary.length*2]);
 	int store = size;
 	for(int i =0;i<size;i++){
-	    nw[i] = (Object)(this.removeFirst());
+	    nw[i] = this.removeFirst();
 	}
 	ary = nw;
 	start =0;
@@ -28,52 +32,84 @@ public class MyDeque<T> {
     }
 
     public T removeFirst(){
-        T v = (T)(ary[start]);
-	if(start == ary.length-1){
-	    start = 0;
-	}else{
-	    start++;
+	if(size == 0){
+	    throw new IndexOutOfBoundsException("Size is zero");
+	}
+        T v = ary[start];
+	ary[start]=null;
+	if(end!= start){
+	    if(start == ary.length-1){	    
+		start = 0;
+	    }else{
+		start++;
+	    }
 	}
 	size--;
 	return v;
     }
     public T removeLast(){
-	T v = (T)(ary[end]);
-	if(end == 0){
-	    end = ary.length-1;
-	}else{
-	    end--;
+	if(size == 0){
+	    throw new IndexOutOfBoundsException("Size is zero");
+	}
+	T v = ary[end];
+	ary[end]=null;
+	if(end!= start){
+	    if(end == 0){
+		end = ary.length-1;
+	    }else{
+		end--;
+	    }
 	}
 	size--;
 	return v;
     }
     public void addFirst(T value){
+	if(size==0){
+	    ary[start] = value;
+	    size++;
+	    return;
+	}
 	if(size >= ary.length){
 	    grow();
 	}
 	if(start == 0){
 	    ary[ary.length-1] = value;
+	    start=ary.length-1;
 	}else{
 	    ary[start-1] = value;
+	    start--;
 	}
 	size++;
     }
     public void addLast(T value){
+	if(size==0){
+	    ary[start] = value;
+	    size++;
+	    return;
+	}
 	if(size >= ary.length){
 	    grow();
 	}
 	if(end == ary.length-1){
 	    ary[0] = value;
+	    end = 0;
 	}else{
 	    ary[end+1] = value;
+	    end++;
 	}
 	size++;
     }
     public T getFirst(){
-	return (T)(ary[start]);
+	if(size == 0){
+	    throw new IndexOutOfBoundsException("Size is zero");
+	}
+	return ary[start];
     }
     public T getLast(){
-	return (T)(ary[end]);
+	if(size == 0){
+	    throw new IndexOutOfBoundsException("Size is zero");
+	}
+	return ary[end];
     }
     
 }
